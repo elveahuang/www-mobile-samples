178582c95a8bf2ef619bc3a885291b74872dae09
Elvea Huang <elvea.huang@gmail.com>
2021-03-05 11:58:14 +0800

    移除重复的Ionic示例

--- ionic-sample/src/app/discover/discover.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-discover',
-    templateUrl: 'discover.page.html',
-    styleUrls: ['discover.page.scss'],
-})
-export class DiscoverPage {}
--- ionic-sample/src/app/me/me.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-me',
-    templateUrl: 'me.page.html',
-    styleUrls: ['me.page.scss'],
-})
-export class MePage {}
